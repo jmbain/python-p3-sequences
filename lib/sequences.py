c1af88e4cfc7ea24cd3a7a2c17ddd1911493d4b8
Josh Bain <joshuambain@gmail.com>
2024-08-29 16:48:35 -0400

    complete sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    result = []
+    if length == 0:
+        print(result)
+    elif length == 1:
+        result.append(0)
+        print(result)
+    elif length == 2:
+        result =[0, 1]
+        print(result)
+    elif length > 2:
+        result = [0, 1]
+        for i in range(2, length):
+            result.append(result[-1] + result[-2])
+        print(result)
